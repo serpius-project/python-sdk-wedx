476b8c1e5cc725dfdc0285d3eb22a1bfa9268282
Miller Mendoza Jimenez <millmen@serpius.ch>
2025-03-09 17:18:38 +0100

    adding it to the script

--- examples/script.sh
+++ examples/script.sh
@@ -2,5 +2,5 @@ while :
 do
     nice -n 19 python3.9 traderProEWArbitrum.py
     nice -n 19 python3.9 traderProEWBase.py
-    sleep 86400
+    sleep 14400
 done
--- examples/script_1.sh
+++ examples/script_1.sh
@@ -2,5 +2,6 @@ while :
 do
     nice -n 19 python3.9 traderProTVLWArbitrum.py
     nice -n 19 python3.9 traderProTVLWBase.py
-    sleep 86400
+    nice -n 19 python3.9 traderProLibertyEthereum.py
+    sleep 14400
 done
