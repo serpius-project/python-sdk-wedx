a7d13f0c1dbaa9511594227c8d9cae5942ce28fa
Miller Mendoza Jimenez <millmen@serpius.ch>
2025-06-11 08:07:00 +0200

    adding change on slippage

--- examples/traderProLibertyEthereum.py
+++ examples/traderProLibertyEthereum.py
@@ -111,11 +111,11 @@ def main():
     w3 = Web3(Web3.HTTPProvider(CHAIN_RPCS[1]))
     gas_price = w3.eth.gas_price
     print(f"Gas price is {gas_price}, permitted max. {w3.to_wei(1.5, 'gwei')}")
-    if gas_price > w3.to_wei(1.5, 'gwei'):
+    if gas_price > w3.to_wei(1.6, 'gwei'):
         update = False           
 
     print(f"My current slippage is {wedx.get_current_slippage()}")
-    wedx.change_slippage(1000000)
+#    wedx.change_slippage(1000000)
 
     if update:
         try:
